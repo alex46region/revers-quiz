fe7fffc16c2a4b64d08bfadefd592844800f6c61
alex46region <alex46region@gmail.com>
2026-08-21 15:49:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,3 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(clients.claim());
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
-});
